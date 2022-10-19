f73decefc8077f78994a5f338af32f1754865487
Andres Maneiro <amaneiro7@gmail.com>
2022-10-19 06:07:23 -0400

    se corrigio la url de la api

--- src/app/Weather.js
+++ src/app/Weather.js
@@ -7,7 +7,7 @@ export class Weather {
     }
 
     async getWeather() {
-        const URI = `http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apikey}&units=metric`;
+        const URI = `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apikey}&units=metric`;
         const response = await fetch(URI);
         const data = await response.json();
         return data;
